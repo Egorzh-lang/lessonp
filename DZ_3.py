bd95f4b098fc7d6e5cbf885c5a99f44082f85f5e
Egorzh-lang <egorii.zhu12@gmail.com>
2024-11-28 21:41:07 +0300

    Create DZ_3.py

---
+++ DZ_3.py
@@ -0,0 +1,16 @@
+n = int(input())
+s = 0
+for i in range(n+1):
+    s = i*i*i + s 
+print(s)
+
+
+for i in range(1, 10):
+    print(f"{i}|", end="")  
+    for j in range(1, 10):
+        if j*i > 9:
+            print(f"  {i * j}", end="")
+        else:
+            print(f"   {i * j}", end="")  
+    print() 
+

8a0072c87dcd9a3a4c2a9ba1dfeb49a959439eb3
Egorzh-lang <egorii.zhu12@gmail.com>
2024-12-05 22:53:11 +0300

    Update DZ_12.py

--- DZ_12.py
+++ DZ_12.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 file = '/content/Mall_Customers.csv'
 df = pd.read_csv(file, sep=',')
 
@@ -16,4 +16,17 @@ plt.ylabel('Spending Score (1-100)')
 plt.title('Spending Score_Age_Male ')
 plt.legend()
 plt.grid(True)
+plt.show()
+###################################################################
+grouped = df.groupby(['Genre', 'Annual Income (k$)'])['Spending Score (1-100)'].mean().unstack()
+
+
+grouped.plot(kind='bar', figsize=(10, 6), color=['blue', 'red']) 
+
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.title('Spending Score (1-100)_Annual Income (k$) by Genre')
+plt.xticks(rotation=0) 
+plt.legend(['Male', 'Female'])
+plt.grid(axis='y')  
 plt.show()
